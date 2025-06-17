a18ea77f9225519a814ebf1218fcc3ddea0399c0
Neloy-SWE <taufiqneloy.swe@gmail.com>
2025-06-17 23:59:19 +0600

    all events are added

---
+++ lib/timer/bloc/timer_event.dart
@@ -0,0 +1,34 @@
+/*
+* Created by Neloy on 17 June, 2025.
+* Email: taufiqneloy.swe@gmail.com
+*/
+
+part of 'timer_bloc.dart';
+
+sealed class TimerEvent {
+  const TimerEvent();
+}
+
+final class TimerStarted extends TimerEvent {
+  final int duration;
+
+  const TimerStarted({required this.duration});
+}
+
+final class TimerPaused extends TimerEvent {
+  const TimerPaused();
+}
+
+final class TimerResumed extends TimerEvent {
+  const TimerResumed();
+}
+
+class TimerReset extends TimerEvent {
+  const TimerReset();
+}
+
+class _TimerTicked extends TimerEvent {
+  final int duration;
+
+  const _TimerTicked({required this.duration});
+}
